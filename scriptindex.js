34d437cdf110c7812d7b111c8a8a9af835868a84
RodrigoBorgesMoura <121907650+RodrigoBorgesMoura@users.noreply.github.com>
2023-02-04 22:04:31 -0300

    Update scriptindex.js

--- scriptindex.js
+++ scriptindex.js
@@ -1,11 +1,1 @@
-function share(){
-	if (navigator.share !== undefined) {
-		navigator.share({
-			title: 'O título da sua página',
-			text: 'Um texto de resumo',
-			url: 'https://rodrigoborgesmoura.github.io/LinkTree/',
-		})
-		.then(() => console.log('Successful share'))
-		.catch((error) => console.log('Error sharing', error));
-	}
-}
+
